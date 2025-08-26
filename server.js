74ba933649fef28f86da1b57ab1ec3dfbfbdd4d1
Kevyn-Marques <takinhoverycrazy@gmail.com>
2025-08-26 12:21:48 -0300

    Put e Delete

--- server.js
+++ server.js
@@ -10,6 +10,34 @@ app.use(express.json())
 
 
 //Rotas
+
+app.delete('/cadastro/:id', async (req,res)=>{
+
+        await prisma.usuario.delete({
+                where:{
+                        id:req.params.id
+                }
+        })
+        res.status(201).json({"mensagem":"Usuario removido"})
+        //console.log(req.params.id)
+})
+
+app.put('/cadastro/:id', async (req,res)=>{
+
+        await prisma.usuario.update({
+                where:{
+                        id:req.params.id
+                },
+                data:{
+                        email: req.body.email,
+                        nome:  req.body.nome,
+                        idade: req.body.idade
+                }
+        })
+        res.status(201).json({"mensagem":"Usuario Atualizado"})
+        //console.log(req.params.id)
+})
+
 app.get('/cadastro',async (req,res)=>{
   const usuarios = await prisma.usuario.findMany();
     res.status(200).json(usuarios)
